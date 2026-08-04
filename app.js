735c1d7b19396f9573e413812e2b71d806cff27c
Liano360 <mareczek.lian@web.de>
2026-08-04 16:01:26 +0200

    app.js aktualisieren

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-a
+const splashScreen = document.getElementById("splashScreen");
+const mainMenu = document.getElementById("mainMenu");
+
+window.addEventListener("load", () => {
+
+    setTimeout(() => {
+
+        splashScreen.style.transition = "opacity 0.8s ease";
+        splashScreen.style.opacity = "0";
+
+        setTimeout(() => {
+
+            splashScreen.style.display = "none";
+
+            mainMenu.style.display = "block";
+            mainMenu.style.opacity = "0";
+            mainMenu.style.transition = "opacity 0.8s ease";
+
+            setTimeout(() => {
+                mainMenu.style.opacity = "1";
+            }, 50);
+
+        }, 800);
+
+    }, 2000);
+
+});
