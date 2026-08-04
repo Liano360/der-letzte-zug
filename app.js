81d83e13ab1920989ff9148d7d42825a9f2b1e37
Liano360 <mareczek.lian@web.de>
2026-08-04 17:15:21 +0200

    app.js aktualisieren

--- app.js
+++ app.js
@@ -1,32 +1,81 @@
 const splashScreen = document.getElementById("splashScreen");
 const mainMenu = document.getElementById("mainMenu");
 
+
+// Startanimation
+
 window.addEventListener("load", () => {
 
-    // Logo bleibt kurz im Mittelpunkt
     setTimeout(() => {
 
-        splashScreen.style.transition = "opacity 1.2s ease";
+        splashScreen.style.transition = "opacity 1s ease";
         splashScreen.style.opacity = "0";
 
-        // Hauptmenü erscheint weich
+
         setTimeout(() => {
 
             splashScreen.style.display = "none";
 
-            mainMenu.style.display = "block";
-            mainMenu.style.opacity = "0";
+            mainMenu.classList.remove("hidden");
 
-            mainMenu.style.transition = "opacity 1.2s ease";
+            mainMenu.style.opacity = "0";
 
             setTimeout(() => {
 
+                mainMenu.style.transition = "opacity 0.8s ease";
                 mainMenu.style.opacity = "1";
 
             }, 100);
 
-        }, 1200);
+
+        }, 1000);
+
 
     }, 3000);
 
 });
+
+
+
+// Bildschirm wechseln
+
+function openScreen(screenID) {
+
+    const screens = document.querySelectorAll(".screen");
+
+
+    screens.forEach(screen => {
+
+        screen.style.opacity = "0";
+
+        setTimeout(() => {
+
+            screen.classList.add("hidden");
+
+        }, 300);
+
+    });
+
+
+
+    setTimeout(() => {
+
+        const nextScreen = document.getElementById(screenID);
+
+        nextScreen.classList.remove("hidden");
+
+        nextScreen.style.opacity = "0";
+
+
+        setTimeout(() => {
+
+            nextScreen.style.transition = "opacity 0.5s ease";
+
+            nextScreen.style.opacity = "1";
+
+        }, 50);
+
+
+    }, 350);
+
+}
