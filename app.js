5333774c673afa545aee6ad407d123e63822aa39
Liano360 <mareczek.lian@web.de>
2026-08-04 17:32:40 +0200

    app.js aktualisieren

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const splashScreen = document.getElementById("splashScreen");
 const mainMenu = document.getElementById("mainMenu");
 
-
-// Startanimation
-
 window.addEventListener("load", () => {
 
     setTimeout(() => {
@@ -16,13 +13,13 @@ window.addEventListener("load", () => {
 
             splashScreen.style.display = "none";
 
-            mainMenu.classList.remove("hidden");
+            mainMenu.style.display = "block";
 
             mainMenu.style.opacity = "0";
 
             setTimeout(() => {
 
-                mainMenu.style.transition = "opacity 0.8s ease";
+                mainMenu.style.transition = "opacity 1s ease";
                 mainMenu.style.opacity = "1";
 
             }, 100);
@@ -31,51 +28,6 @@ window.addEventListener("load", () => {
         }, 1000);
 
 
-    }, 3000);
+    }, 4500);
 
 });
-
-
-
-// Bildschirm wechseln
-
-function openScreen(screenID) {
-
-    const screens = document.querySelectorAll(".screen");
-
-
-    screens.forEach(screen => {
-
-        screen.style.opacity = "0";
-
-        setTimeout(() => {
-
-            screen.classList.add("hidden");
-
-        }, 300);
-
-    });
-
-
-
-    setTimeout(() => {
-
-        const nextScreen = document.getElementById(screenID);
-
-        nextScreen.classList.remove("hidden");
-
-        nextScreen.style.opacity = "0";
-
-
-        setTimeout(() => {
-
-            nextScreen.style.transition = "opacity 0.5s ease";
-
-            nextScreen.style.opacity = "1";
-
-        }, 50);
-
-
-    }, 350);
-
-}
